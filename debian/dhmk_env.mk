e3829687f5bea53978ddcd347943119fd3951756
hahn <matcherapy@gmail.com>
2018-04-28 10:34:39 +0700

    gcompris 0.90

---
+++ debian/dhmk_env.mk
@@ -0,0 +1,9 @@
+export CFLAGS := -g -O2 -fdebug-prefix-map=/home/h/Dokumen/dev/gcompris/gcompris-qt=. -fstack-protector-strong -Wformat -Werror=format-security -Wall -pedantic
+export CPPFLAGS := -Wdate-time -D_FORTIFY_SOURCE=2
+export CXXFLAGS := -g -O2 -fdebug-prefix-map=/home/h/Dokumen/dev/gcompris/gcompris-qt=. -fstack-protector-strong -Wformat -Werror=format-security
+export FCFLAGS := -g -O2 -fdebug-prefix-map=/home/h/Dokumen/dev/gcompris/gcompris-qt=. -fstack-protector-strong
+export FFLAGS := -g -O2 -fdebug-prefix-map=/home/h/Dokumen/dev/gcompris/gcompris-qt=. -fstack-protector-strong
+export GCJFLAGS := -g -O2 -fdebug-prefix-map=/home/h/Dokumen/dev/gcompris/gcompris-qt=. -fstack-protector-strong
+export LDFLAGS := -Wl,-z,relro -Wl,-z,now -Wl,--as-needed
+export OBJCFLAGS := -g -O2 -fdebug-prefix-map=/home/h/Dokumen/dev/gcompris/gcompris-qt=. -fstack-protector-strong -Wformat -Werror=format-security
+export OBJCXXFLAGS := -g -O2 -fdebug-prefix-map=/home/h/Dokumen/dev/gcompris/gcompris-qt=. -fstack-protector-strong -Wformat -Werror=format-security
